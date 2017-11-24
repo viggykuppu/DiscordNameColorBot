7ce5c4fd37f62884423f3e32f8a0efef82f9d5d1
viggykuppu <viggykuppu@gmail.com>
2017-11-24 10:20:40 -0500

    First shopass at adding error handling

--- main.go
+++ main.go
@@ -18,30 +18,26 @@ func main() {
 
 	discord, err := discordgo.New("Bot " + authToken)
 
-  if err != nil {
-    fmt.Println("Error creating discord session: ", err)
-  }
-
   discord.AddHandler(messageCreate)
 
   // Open the websocket and begin listening.
   err = discord.Open()
 
-  err = discord.UpdateStatus(0,"Type *help")
-
   if err != nil {
     fmt.Println("Error opening Discord session: ", err)
+  } else {
+    err = discord.UpdateStatus(0,"Type *help")
+
+    // Wait here until CTRL-C or other term signal is received.
+    fmt.Println("Discord name color bot.  Press CTRL-C to exit.")
+    sc := make(chan os.Signal, 1)
+    signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+    <-sc
+
+    // Cleanly close down the Discord session.
+    fmt.Println("Closing bot... goodbye!")
+    discord.Close()
   }
-
-  // Wait here until CTRL-C or other term signal is received.
-  fmt.Println("Discord name color bot.  Press CTRL-C to exit.")
-  sc := make(chan os.Signal, 1)
-  signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-  <-sc
-
-  // Cleanly close down the Discord session.
-  fmt.Println("Closing bot... goodbye!")
-  discord.Close()
 }
 
 func parseConfig(){
@@ -93,42 +89,34 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 }
 
 func processColorCommand(pieces []string, s *discordgo.Session, msg *discordgo.Message) {
+  defer handleError()
   if len(pieces) == 2 {
-    color, success := parseColor(pieces[1])
-    if success {
-      // TODO: There should be an easier way to get the guild id
-      guildID := getGuildID(s, msg.ChannelID)
-      userID := msg.Author.ID
+    color := parseColor(pieces[1])
+    // TODO: There should be an easier way to get the guild id
+    guildID := getGuildID(s, msg.ChannelID)
+    userID := msg.Author.ID
 
-      setNameColor(s, guildID, userID, color)
+    setNameColor(s, guildID, userID, color)
 
-      // directChannel, _ := s.UserChannelCreate(userID)
-      // var messageContent = msg.Author.Username + ", your color has now been set to " + pieces[1]
-      // s.ChannelMessageSend(directChannel.ID, messageContent)
-    } else {
-      fmt.Println("failed to parse color")
-    }
+    // directChannel, _ := s.UserChannelCreate(userID)
+    // var messageContent = msg.Author.Username + ", your color has now been set to " + pieces[1]
+    // s.ChannelMessageSend(directChannel.ID, messageContent)
   } else {
     fmt.Println("failed to parse color")
   }
 }
 
 func processHelpCommand(s *discordgo.Session, msg *discordgo.Message) {
+  defer handleError()
   directChannel, err := s.UserChannelCreate(msg.Author.ID)
-  if err != nil {
-    fmt.Println("failed creating dm channel: ", err)
-  } else {
-    var messageContent = "Please enter a command of the following format:\n\t***color <color_value>**\n\tWhere color value is of the format *#<hex_value>*, *0x<hex_value>*, or *<decimal_value>*\n\tYou can find the corresponding hex values for colors here: https://www.w3schools.com/colors/colors_picker.asp"
-    s.ChannelMessageSend(directChannel.ID, messageContent)
-  }
+  check(err)
+  var messageContent = "Please enter a command of the following format:\n\t***color <color_value>**\n\tWhere color value is of the format *#<hex_value>*, *0x<hex_value>*, or *<decimal_value>*\n\tYou can find the corresponding hex values for colors here: https://www.w3schools.com/colors/colors_picker.asp"
+  s.ChannelMessageSend(directChannel.ID, messageContent)
 }
 
 func getGuildID(s *discordgo.Session, channelID string) string {
   channel, err := s.Channel(channelID)
-  if err != nil {
-    fmt.Println("error retrieving channel to get guild ID", err)
-    return ""
-  }
+  check(err)
   return channel.GuildID
 }
 
@@ -144,40 +132,37 @@ func setNameColor(s *discordgo.Session, guildID string, userID string, color int
     s.GuildRoleEdit(guildID, existingRole.ID, existingRole.Name, color, existingRole.Hoist, existingRole.Permissions, existingRole.Mentionable)
   } else {
     newRole, err := s.GuildRoleCreate(guildID)
-    if err != nil {
-      fmt.Println("error creating new role", err)
-    } else {
-      s.GuildRoleEdit(guildID, newRole.ID, userID + "'s color role", color, false, 0, false)
-      s.GuildMemberRoleAdd(guildID, userID, newRole.ID)
-    }
+    check(err)
+    s.GuildRoleEdit(guildID, newRole.ID, userID + "'s color role", color, false, 0, false)
+    s.GuildMemberRoleAdd(guildID, userID, newRole.ID)
   }
 }
 
-func parseColor(color string) (int, bool) {
+func parseColor(color string) int {
   var decimalValue int
   if strings.HasPrefix(color, "#") {
     decimalValue64, err := strconv.ParseInt(color[1:len(color)], 16, 32)
     decimalValue = int(decimalValue64)
-    if err != nil {
-      decimalValue = -1
-    }
+    check(err)
   } else if strings.HasPrefix(color, "0x") {
     decimalValue64, err := strconv.ParseInt(color, 0, 32)
     decimalValue = int(decimalValue64)
-    if err != nil {
-      decimalValue = -1
-    }
+    check(err)
   } else {
     decimalValue64, err := strconv.ParseInt(color, 10, 32)
     decimalValue = int(decimalValue64)
-    if err != nil {
-      decimalValue = -1
-    }
+    check(err)
   }
   
   if decimalValue >= 0 && decimalValue <= 16777215 {
-    return decimalValue, true
+    return decimalValue
   } else {
-    return -1, false
+    panic(-1)
+  }
+}
+
+func handleError(){
+  if r := recover(); r != nil {
+    fmt.Println("Error", r)
   }
 }
